c00f1ac4ecccc199d6d6115f9dd7dc99577aa3fe
Rainald Durand <rainald.durand-ext@yrnet.com>
2023-12-15 10:52:27 +0100

    rename file and cli arg

--- cmd/root.go
+++ cmd/root.go
@@ -43,5 +43,5 @@ func init() {
 	// Cobra also supports local flags, which will only run
 	// when this action is called directly.
 	rootCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
-	randomCmd.PersistentFlags().String("term", "", "A search term for a dad joke.")
+	dadjokeCmd.PersistentFlags().String("term", "", "A search term for a dad joke.")
 }
