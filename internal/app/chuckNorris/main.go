f8cda378a2f3dedb731ba9faeae5f68c05fefb8b
Rainald Durand <rainald.durand-ext@yrnet.com>
2023-12-15 19:34:50 +0100

    move api request logic to an external package

--- internal/app/chuckNorris/main.go
+++ internal/app/chuckNorris/main.go
@@ -3,10 +3,9 @@ package chucknorris
 import (
 	"encoding/json"
 	"fmt"
-	"io"
 	"log"
-	"net/http"
 	"strings"
+	"tools-cli/internal/api"
 )
 
 const api_url = "https://api.chucknorris.io/jokes/"
@@ -17,17 +16,17 @@ type Phrase struct {
 }
 
 func RandomPhrase() {
-	responseBytes := getDataFromApi(api_url + "random")
+	responseBytes := api.GetDataFromApi(api_url + "random")
 	phrase := Phrase{}
 	if err := json.Unmarshal(responseBytes, &phrase); err != nil {
 		fmt.Printf("Could not unmarshal responseByte. %v", err)
 	}
 
-	fmt.Println(string(phrase.Phrase))
+	fmt.Printf(phrase.Phrase)
 }
 
 func ListAllCategories() {
-	rspBytes := getDataFromApi(api_url + "categories")
+	rspBytes := api.GetDataFromApi(api_url + "categories")
 	var cat []string
 
 	if err := json.Unmarshal(rspBytes, &cat); err != nil {
@@ -38,42 +37,11 @@ func ListAllCategories() {
 }
 
 func GetPhraseByCategory(category string) {
-	rspBytes := getDataFromApi(api_url + "random?category=" + category)
+	rspBytes := api.GetDataFromApi(api_url + "random?category=" + category)
 	phrase := Phrase{}
 
 	if err := json.Unmarshal(rspBytes, &phrase); err != nil {
 		log.Printf("Could not unmarshall responseBytes. %v", err)
 	}
-
 	fmt.Printf(phrase.Phrase)
-
-}
-
-func getDataFromApi(url string) []byte {
-	r, err := http.NewRequest(
-		http.MethodGet,
-		url,
-		nil,
-	)
-
-	if err != nil {
-		log.Printf("Could not create a new request to the api. %v", err)
-	}
-
-	r.Header.Add("Accept", "application/json")
-	r.Header.Add("User-agent", "Joke CLI (https://github.com/Raindevops/Joke-cli)")
-
-	rsp, err := http.DefaultClient.Do(r)
-
-	if err != nil {
-		log.Printf("Could not make a request. %v", err)
-	}
-
-	rspBytes, err := io.ReadAll(rsp.Body)
-
-	if err != nil {
-		log.Printf("Could not read response Body. %v", err)
-	}
-
-	return rspBytes
 }
